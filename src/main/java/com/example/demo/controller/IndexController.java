d54423920cdcd11441792d40d8a16b93f8543ad4
TruongDX8 <truonghusk17@gmail.com>
2020-07-01 20:57:06 +0700

    first commit

---
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -0,0 +1,29 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.User;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+public class IndexController {
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @RequestMapping("/hello")
+    public String hello(Model model){
+        List<User> lsUser = userService.getAll();
+        System.out.println("Size: "+lsUser.size());
+        model.addAttribute("hello","truong");
+        return "hello";
+    }
+}
